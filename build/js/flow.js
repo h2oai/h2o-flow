a9ba60849cd8ee7baf83b4a7385307f570517585
Arno Candel <arno.candel@gmail.com>
2015-11-23 11:16:29 -0800

    Release 0.4.2

--- build/js/flow.js
+++ build/js/flow.js
@@ -12,7 +12,7 @@
     }
 }.call(this));
 (function () {
-    Flow.Version = '0.4.1';
+    Flow.Version = '0.4.2';
     Flow.About = function (_) {
         var _properties;
         _properties = Flow.Dataflow.signals([]);
