b7a1a4c7c4addd1e7ab9c0a87c183c914416ea3c
Prithvi Prabhu <prithvi@0xdata.com>
2016-02-29 17:32:09 -0800

    Release 0.4.19

--- build/js/headless-test.js
+++ build/js/headless-test.js
@@ -1,4 +1,4 @@
-var hostname, opts, packNames, packsArg, page, parseOpts, printUsageAndExit, system, timeout, timeoutArg, waitFor, webpage, _ref;
+var excludeFlowName, excludeFlowsArg, excludeFlowsNames, hostname, opts, packNames, packsArg, page, parseOpts, printUsageAndExit, system, timeout, timeoutArg, waitFor, webpage, _i, _len, _ref;
 
 system = require('system');
 
@@ -23,47 +23,124 @@ phantom.onError = function(message, stacktrace) {
 
 printUsageAndExit = function(message) {
   console.log("*** " + message + " ***");
-  console.log('Usage: phantomjs headless-test.js [--host ip:port] [--timeout seconds] --packs foo:bar:baz');
-  console.log('    ip:port  defaults to localhost:54321');
-  console.log('    timeout  defaults to 3600');
+  console.log('Usage: phantomjs headless-test.js [--host ip:port] [--timeout seconds] [--packs foo:bar:baz] [--perf date buildId gitHash gitBranch ncpu os jobName outputDir] [--excludeFlows flow1;flow2]');
+  console.log('    ip:port      defaults to localhost:54321');
+  console.log('    timeout      defaults to 3600');
+  console.log('    packs        defaults to examples');
+  console.log('    perf         performance of individual tests will be recorded in perf.csv in the output directory');
+  console.log('    excludeFlows do not run these flows');
   return phantom.exit(1);
 };
 
 parseOpts = function(args) {
-  var i, key, opts, _i, _len;
+  var i, opts;
   console.log("Using args " + (args.join(' ')));
-  if (args.length % 2 === 1) {
-    printUsageAndExit('Expected even number of command line arguments');
-  }
+  i = 0;
   opts = {};
-  for (i = _i = 0, _len = args.length; _i < _len; i = ++_i) {
-    key = args[i];
-    if (!(i % 2 === 0)) {
-      continue;
+  while (i < args.length) {
+    if (args[i] === "--host") {
+      i = i + 1;
+      if (i > args.length) {
+        printUsageAndExit("Unknown argument: " + args[i]);
+      }
+      opts['hostname'] = args[i];
+    } else if (args[i] === "--timeout") {
+      i = i + 1;
+      if (i > args.length) {
+        printUsageAndExit("Unknown argument: " + args[i]);
+      }
+      opts['timeout'] = args[i];
+    } else if (args[i] === "--packs") {
+      i = i + 1;
+      if (i > args.length) {
+        printUsageAndExit("Unknown argument: " + args[i]);
+      }
+      opts['packs'] = args[i];
+    } else if (args[i] === "--perf") {
+      opts['perf'] = true;
+      i = i + 1;
+      if (i > args.length) {
+        printUsageAndExit("Unknown argument: " + args[i]);
+      }
+      opts['date'] = args[i];
+      i = i + 1;
+      if (i > args.length) {
+        printUsageAndExit("Unknown argument: " + args[i]);
+      }
+      opts['buildId'] = args[i];
+      i = i + 1;
+      if (i > args.length) {
+        printUsageAndExit("Unknown argument: " + args[i]);
+      }
+      opts['gitHash'] = args[i];
+      i = i + 1;
+      if (i > args.length) {
+        printUsageAndExit("Unknown argument: " + args[i]);
+      }
+      opts['gitBranch'] = args[i];
+      i = i + 1;
+      if (i > args.length) {
+        printUsageAndExit("Unknown argument: " + args[i]);
+      }
+      opts['ncpu'] = args[i];
+      i = i + 1;
+      if (i > args.length) {
+        printUsageAndExit("Unknown argument: " + args[i]);
+      }
+      opts['os'] = args[i];
+      i = i + 1;
+      if (i > args.length) {
+        printUsageAndExit("Unknown argument: " + args[i]);
+      }
+      opts['jobName'] = args[i];
+      i = i + 1;
+      if (i > args.length) {
+        printUsageAndExit("Unknown argument: " + args[i]);
+      }
+      opts['outputDir'] = args[i];
+    } else if (args[i] === "--excludeFlows") {
+      i = i + 1;
+      if (i > args.length) {
+        printUsageAndExit("Unknown argument: " + args[i]);
+      }
+      opts['excludeFlows'] = args[i];
+    } else {
+      printUsageAndExit("Unknown argument: " + args[i]);
     }
-    if (key.slice(0, 2) !== '--') {
-      return printUsageAndExit("Expected keyword. Found " + key);
-    }
-    opts[key] = args[i + 1];
+    i = i + 1;
   }
   return opts;
 };
 
 opts = parseOpts(system.args.slice(1));
 
-hostname = (_ref = opts['--host']) != null ? _ref : 'localhost:54321';
+hostname = (_ref = opts['hostname']) != null ? _ref : 'localhost:54321';
 
 console.log("PHANTOM: Using " + hostname);
 
-timeout = (timeoutArg = opts['--timeout']) ? 1000 * parseInt(timeoutArg, 10) : 3600000;
+timeout = (timeoutArg = opts['timeout']) ? 1000 * parseInt(timeoutArg, 10) : 3600000;
 
 console.log("PHANTOM: Using timeout " + timeout + "ms");
 
-packsArg = opts['--packs'];
+packsArg = opts['packs'];
 
 packNames = packsArg ? packsArg.split(':') : ['examples'];
 
+excludeFlowsArg = opts['excludeFlows'];
+
+excludeFlowsNames = excludeFlowsArg ? excludeFlowsArg.split(';') : [];
+
+for (_i = 0, _len = excludeFlowsNames.length; _i < _len; _i++) {
+  excludeFlowName = excludeFlowsNames[_i];
+  console.log("PHANTOM: Excluding flow: " + excludeFlowName);
+}
+
 page = webpage.create();
+
+if (opts['perf']) {
+  console.log("PHANTOM: Performance of individual tests will be recorded in perf.csv in output directory: " + opts['outputDir'] + ".");
+  page._outputDir = opts['outputDir'];
+}
 
 page.onResourceError = function(_arg) {
   var errorString, url;
@@ -73,6 +150,12 @@ page.onResourceError = function(_arg) {
 
 page.onConsoleMessage = function(message) {
   return console.log("BROWSER: " + message);
+};
+
+page.onCallback = function(perfLine) {
+  var fs;
+  fs = require('fs');
+  return fs.write(page._outputDir + '/perf.csv', perfLine, 'a');
 };
 
 waitFor = function(test, onReady) {
@@ -100,8 +183,18 @@ page.open("http://" + hostname + "/flow/index.html", function(status) {
   var printErrors, test;
   if (status === 'success') {
     test = function() {
-      return page.evaluate(function(packNames) {
+      return page.evaluate(function(packNames, date, buildId, gitHash, gitBranch, hostname, ncpu, os, jobName, perf, excludeFlowsNames) {
         var context, runFlow, runPack, runPacks;
+        window._date = date;
+        window._buildId = buildId;
+        window._gitHash = gitHash;
+        window._gitBranch = gitBranch;
+        window._hostname = hostname;
+        window._ncpu = ncpu;
+        window._os = os;
+        window._jobName = jobName;
+        window._perf = perf;
+        window._excludeFlowsNames = excludeFlowsNames;
         context = window.flow.context;
         if (window._phantom_started_) {
           if (window._phantom_exit_) {
@@ -139,45 +232,67 @@ page.open("http://" + hostname + "/flow/index.html", function(status) {
             });
           };
           runFlow = function(packName, flowName, go) {
-            var flowTitle;
-            flowTitle = "" + packName + " - " + flowName;
-            window._phantom_test_summary_[flowTitle] = 'FAILED';
-            console.log("Fetching flow document: " + packName + " - " + flowName + "...");
-            return context.requestFlow(packName, flowName, function(error, flow) {
-              var waitForFlow;
-              if (error) {
-                console.log("*** ERROR *** Failed fetching flow " + flowTitle);
-                return go(new Error("Failed fetching flow " + flowTitle, error));
-              } else {
-                console.log("Opening flow " + flowTitle + "...");
-                window._phantom_running_ = true;
-                context.open(flowTitle, flow);
-                waitForFlow = function() {
-                  var errors;
-                  if (window._phantom_running_) {
-                    console.log('ACK');
-                    return setTimeout(waitForFlow, 2000);
-                  } else {
-                    console.log('Flow completed!');
-                    errors = window._phantom_errors_;
-                    return context.requestRemoveAll(function() {
-                      return go(errors ? errors : null);
-                    });
-                  }
-                };
-                console.log('Running flow...');
-                context.executeAllCells(true, function(status, errors) {
-                  console.log("Flow finished with status: " + status);
-                  if (status === 'failed') {
-                    window._phantom_errors_ = errors;
-                  } else {
-                    window._phantom_test_summary_[flowTitle] = 'PASSED';
-                  }
-                  return window._phantom_running_ = false;
-                });
-                return setTimeout(waitForFlow, 2000);
+            var doFlow, flowTitle;
+            doFlow = function(flowName, excludeFlowsNames) {
+              var f, _j, _len1;
+              for (_j = 0, _len1 = excludeFlowsNames.length; _j < _len1; _j++) {
+                f = excludeFlowsNames[_j];
+                if (flowName === f) {
+                  return false;
+                }
               }
-            });
+              return true;
+            };
+            if (doFlow(flowName, window._excludeFlowsNames)) {
+              flowTitle = "" + packName + " - " + flowName;
+              window._phantom_test_summary_[flowTitle] = 'FAILED';
+              console.log("Fetching flow document: " + packName + " - " + flowName + "...");
+              return context.requestFlow(packName, flowName, function(error, flow) {
+                var waitForFlow;
+                if (error) {
+                  console.log("*** ERROR *** Failed fetching flow " + flowTitle);
+                  go(new Error("Failed fetching flow " + flowTitle, error));
+                } else {
+                  console.log("Opening flow " + flowTitle + "...");
+                  window._phantom_running_ = true;
+                  context.open(flowTitle, flow);
+                  waitForFlow = function() {
+                    var errors;
+                    if (window._phantom_running_) {
+                      console.log('ACK');
+                      return setTimeout(waitForFlow, 2000);
+                    } else {
+                      console.log('Flow completed!');
+                      errors = window._phantom_errors_;
+                      return context.requestRemoveAll(function() {
+                        return go(errors ? errors : null);
+                      });
+                    }
+                  };
+                  console.log('Running flow...');
+                  window._startTime = new Date().getTime() / 1000;
+                  context.executeAllCells(true, function(status, errors) {
+                    window._endTime = new Date().getTime() / 1000;
+                    console.log("Flow finished with status: " + status);
+                    if (status === 'failed') {
+                      window._pass = 0;
+                      window._phantom_errors_ = errors;
+                    } else {
+                      window._pass = 1;
+                      window._phantom_test_summary_[flowTitle] = 'PASSED';
+                    }
+                    if (window._perf) {
+                      window.callPhantom("" + window._date + ", " + window._buildId + ", " + window._gitHash + ", " + window._gitBranch + ", " + window._hostname + ", " + flowName + ", " + window._startTime + ", " + window._endTime + ", " + window._pass + ", " + window._ncpu + ", " + window._os + ", " + window._jobName + "\n");
+                    }
+                    return window._phantom_running_ = false;
+                  });
+                }
+                return setTimeout(waitForFlow, 2000);
+              });
+            } else {
+              console.log("Ignoring flow: " + flowName);
+              return go(null);
+            }
           };
           console.log('Starting tests...');
           window._phantom_errors_ = null;
@@ -194,7 +309,7 @@ page.open("http://" + hostname + "/flow/index.html", function(status) {
           });
           return false;
         }
-      }, packNames);
+      }, packNames, opts['date'], opts['buildId'], opts['gitHash'], opts['gitBranch'], hostname, opts['ncpu'], opts['os'], opts['jobName'], opts['perf'], excludeFlowsNames);
     };
     printErrors = function(errors, prefix) {
       var error;
@@ -204,10 +319,10 @@ page.open("http://" + hostname + "/flow/index.html", function(status) {
       if (errors) {
         if (Array.isArray(errors)) {
           return ((function() {
-            var _i, _len, _results;
+            var _j, _len1, _results;
             _results = [];
-            for (_i = 0, _len = errors.length; _i < _len; _i++) {
-              error = errors[_i];
+            for (_j = 0, _len1 = errors.length; _j < _len1; _j++) {
+              error = errors[_j];
               _results.push(printErrors(error, prefix + '  '));
             }
             return _results;
