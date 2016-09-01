9bdd7cc6e9c9de1b618029146a9ae063335fbcd5
michalkurka <michalk@h2o.ai>
2016-09-01 15:28:14 -0400

    Release 0.4.47

--- release.sh
+++ release.sh
@@ -2,9 +2,9 @@ set -e
 
 FLOW_VERSION="$1"
 
-git pull --rebase
+#git pull --rebase
 
-npm run build
+#npm run build
 
 sed -E "s/.+version.+/  \"version\": \"$FLOW_VERSION\",/" bower.json > bower.json.tmp && mv bower.json.tmp bower.json
 sed -E "s/999\.999\.999/$FLOW_VERSION/" build/js/flow.js > build/js/flow.js.tmp && mv build/js/flow.js.tmp build/js/flow.js
