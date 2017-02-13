fd4e0c12abc21deee640a2ccf643bc6c56c5055b
Micah Stubbs <micah.stubbs@gmail.com>
2017-02-13 12:24:10 -0800

    abstract out parseOpts

--- test/index.js
+++ test/index.js
@@ -1,4 +1,5 @@
 import printUsageAndExit from './printUsageAndExit';
+import parseOpts from './parseOpts';
 
 var excludeFlowName, excludeFlowsArg, excludeFlowsNames, hostname, opts, packNames, packsArg, page, parseOpts, printUsageAndExit, system, timeout, timeoutArg, waitFor, webpage, _i, _len, _ref;
 
@@ -24,88 +25,7 @@ phantom.onError = function(message, stacktrace) {
   }
 };
 
-parseOpts = function(args) {
-  console.log('parseOpts was called');
-  var i, opts;
-  console.log("Using args " + (args.join(' ')));
-  i = 0;
-  opts = {};
-  while (i < args.length) {
-    if (args[i] === "--host") {
-      i = i + 1;
-      if (i > args.length) {
-        printUsageAndExit(phantom, "Unknown argument: " + args[i]);
-      }
-      opts['hostname'] = args[i];
-    } else if (args[i] === "--timeout") {
-      i = i + 1;
-      if (i > args.length) {
-        printUsageAndExit(phantom, "Unknown argument: " + args[i]);
-      }
-      opts['timeout'] = args[i];
-    } else if (args[i] === "--packs") {
-      i = i + 1;
-      if (i > args.length) {
-        printUsageAndExit(phantom, "Unknown argument: " + args[i]);
-      }
-      opts['packs'] = args[i];
-    } else if (args[i] === "--perf") {
-      opts['perf'] = true;
-      i = i + 1;
-      if (i > args.length) {
-        printUsageAndExit(phantom, "Unknown argument: " + args[i]);
-      }
-      opts['date'] = args[i];
-      i = i + 1;
-      if (i > args.length) {
-        printUsageAndExit(phantom, "Unknown argument: " + args[i]);
-      }
-      opts['buildId'] = args[i];
-      i = i + 1;
-      if (i > args.length) {
-        printUsageAndExit(phantom, "Unknown argument: " + args[i]);
-      }
-      opts['gitHash'] = args[i];
-      i = i + 1;
-      if (i > args.length) {
-        printUsageAndExit(phantom, "Unknown argument: " + args[i]);
-      }
-      opts['gitBranch'] = args[i];
-      i = i + 1;
-      if (i > args.length) {
-        printUsageAndExit(phantom, "Unknown argument: " + args[i]);
-      }
-      opts['ncpu'] = args[i];
-      i = i + 1;
-      if (i > args.length) {
-        printUsageAndExit(phantom, "Unknown argument: " + args[i]);
-      }
-      opts['os'] = args[i];
-      i = i + 1;
-      if (i > args.length) {
-        printUsageAndExit(phantom, "Unknown argument: " + args[i]);
-      }
-      opts['jobName'] = args[i];
-      i = i + 1;
-      if (i > args.length) {
-        printUsageAndExit(phantom, "Unknown argument: " + args[i]);
-      }
-      opts['outputDir'] = args[i];
-    } else if (args[i] === "--excludeFlows") {
-      i = i + 1;
-      if (i > args.length) {
-        printUsageAndExit(phantom, "Unknown argument: " + args[i]);
-      }
-      opts['excludeFlows'] = args[i];
-    } else {
-      printUsageAndExit(phantom, "Unknown argument: " + args[i]);
-    }
-    i = i + 1;
-  }
-  return opts;
-};
-
-opts = parseOpts(system.args.slice(1));
+opts = parseOpts(phantom, system.args.slice(1));
 
 hostname = (_ref = opts['hostname']) != null ? _ref : 'localhost:54321';
 
